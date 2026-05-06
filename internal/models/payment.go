f07b14c6a3b9c56f6234dd87d85527c9ee0f10f4
Veoler <iiterhoev@gmail.com>
2026-05-06 10:50:43 +0300

    Первый Коммит. Каркас проекта

---
+++ internal/models/payment.go
@@ -0,0 +1,7 @@
+package models
+
+import "gorm.io/gorm"
+
+type Payment struct {
+	gorm.Model
+}
---
+++ internal/transport/routes.go
@@ -0,0 +1,13 @@
+package transport
+
+import (
+	"github.com/gin-gonic/gin"
+	// "github.com/Veoler/team-pharmacy/internal/services"
+)
+
+func RegisterRoutes(
+	router *gin.Engine,
+
+) {
+
+}
