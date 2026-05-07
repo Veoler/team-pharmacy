85bf68e549dd40b42e796eafc8dccd424ab1c64b
Veoler <iiterhoev@gmail.com>
2026-05-07 12:09:42 +0300

    Сделал модели Paymetn, Promocode, Review

--- internal/repository/payment_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import (
-	// "github.com/Veoler/team-pharmacy/internal/models"
-	// "gorm.io/gorm"
-)
-
--- internal/transport/payment_handler.go
+++
@@ -1,6 +0,0 @@
-package transport
-
-import (
-	// "github.com/Veoler/team-pharmacy/internal/models"
-	// "github.com/Veoler/team-pharmacy/internal/services"
-)
