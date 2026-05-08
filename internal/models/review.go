66aab34902bff499f252502fb2fa4c78e766d601
Veoler <iiterhoev@gmail.com>
2026-05-08 17:26:18 +0300

    Расскоментил поля

--- internal/models/review.go
+++ internal/models/review.go
@@ -4,19 +4,21 @@ import (
 	"gorm.io/gorm"
 )
 
+// Закомментируйте поля которых пока еще не готовы и красным у вас горят
+
 type Review struct {
 	gorm.Model
-	// User		*User		`json:"-"`
-	// UserID		uint		`json:"user_id"	gorm:"not null;index"`
-	// Medicine	*Medicine	`json:"-"`
-	// MedicineID 	uint		`json:"medicine_id" gorm:"not null;index"`
+	User		*User		`json:"-"`
+	UserID		uint		`json:"user_id" gorm:"not null;index"`
+	Medicine	*Medicine	`json:"-"`
+	MedicineID 	uint		`json:"medicine_id" gorm:"not null;index"`
 	Rating		uint		`json:"rating" gorm:"not null"`				// целое число от 1 до 5;
 	Text		string		`json:"text" gorm:"type:text;not null"`		// текст отзыва;
 }
 
 type ReviewCreateRequest struct {
-	// UserID		uint		`json:"user_id"`
-	// MedicineID 	uint		`json:"medicine_id"`
+	UserID		uint		`json:"user_id"`
+	MedicineID 	uint		`json:"medicine_id"`
 	Rating		uint		`json:"rating"`
 	Text		string		`json:"text"`
 }
