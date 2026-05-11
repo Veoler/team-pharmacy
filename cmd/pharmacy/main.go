2448dd37e51af96d0f583d4dcde19929e86c0a25
Veoler <iiterhoev@gmail.com>
2026-05-11 17:21:30 +0300

    Придал правильный вид остльной части кода

--- cmd/pharmacy/main.go
+++ cmd/pharmacy/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"log"
-
+	"github.com/gin-gonic/gin"
 	"github.com/Veoler/team-pharmacy/internal/config"
 	"github.com/Veoler/team-pharmacy/internal/models"
-	"github.com/Veoler/team-pharmacy/internal/repository"
-	"github.com/Veoler/team-pharmacy/internal/services"
-	"github.com/gin-gonic/gin"
-
 	// "github.com/Veoler/team-pharmacy/internal/repository"
 	// "github.com/Veoler/team-pharmacy/internal/services"
 	"github.com/Veoler/team-pharmacy/internal/transport"
@@ -17,21 +12,13 @@ import (
 func main() {
 	db := config.SetUpDatabaseConnection()
 
-	if err := db.AutoMigrate(&models.User{}, &models.Order{}, &models.OrderItem{}, &models.Cart{}, &models.CartItem{}); err != nil {
+	if err := db.AutoMigrate(&models.Payment{}); err != nil {
 		log.Fatalf("не удалось выполнить миграции: %v", err)
 	}
 
 	router := gin.Default()
 
-	userRepo := repository.NewUserRepository(db)
-	cartRepo := repository.NewCartRepository(db)
-	orderRepo := repository.NewOrderRepository(db)
-
-	userService := services.NewUserService(userRepo)
-	cartService := services.NewCartService(cartRepo, userRepo)
-	orderService := services.NewOrderService(orderRepo, cartRepo, userRepo)
-
-	transport.RegisterRoutes(router, userService, cartService, orderService)
+	transport.RegisterRoutes(router)
 
 	if err := router.Run(); err != nil {
 		log.Fatalf("не удалось запустить HTTP-сервер: %v", err)
