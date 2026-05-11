12aea7f1cd87e2dd38ce90b7a41e1d1a299dfce9
Veoler <iiterhoev@gmail.com>
2026-05-11 17:15:19 +0300

    исправил косяк при создании новой ветки

---
+++ internal/repository/payment_repository.go
@@ -0,0 +1,7 @@
+package repository
+
+import (
+	// "github.com/Veoler/team-pharmacy/internal/models"
+	// "gorm.io/gorm"
+)
+
---
+++ internal/services/payment_service.go
@@ -0,0 +1,7 @@
+  package services
+  
+  import (
+	// "github.com/Veoler/team-pharmacy/internal/models"
+	// "github.com/Veoler/team-pharmacy/internal/repository"
+	// "gorm.io/gorm"
+  )
